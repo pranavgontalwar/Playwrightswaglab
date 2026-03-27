3a04b8202c7a84ceca2826d15d62ed486c7c5408
pranavgontalwar <53423303+pranavgontalwar@users.noreply.github.com>
2026-03-27 12:48:06 +0530

    Update playwright.config.ts
    report add/chnage

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
 
   testDir: './tests',
-  reporter: [['html', { open: 'never' }]],
+  //reporter: [['html', { open: 'never' }]],
   outputDir: 'test-results',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  //reporter: [['list'], ['junit',{ outputFile:'result.xml'}]],
+  reporter: [['list'], ['junit',{ outputFile:'result.xml'}]],
   //reporter: 'html',
   
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
